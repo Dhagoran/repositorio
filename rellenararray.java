f85e8042ebe0af2812092429c70acd446d8c4c5f
Dhagoran <anaruiz201068@gmail.com>
2026-04-14 10:15:06 +0200

    operararrays
    "Primer commit con nombre rellenararray"

---
+++ rellenararray.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.github;
+
+/**
+ *
+ * @author Ana
+ */
+import java.util.Random;
+public class rellenararray {
+
+    int[] array = new int[10];
+
+    public void rellenararray() {
+        Random entrada = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = entrada.nextInt(20) + 1; 
+        }
+    }
+}
+
