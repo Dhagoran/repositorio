8b975aa6fd691c812c1b60c1b43dc497d6d98f7b
Ana <anaruiz201068@gmail.com>
2026-04-14 10:30:01 +0200

    Añadido imprimirarray y nuevometodo1

--- rellenararray.java
+++ rellenararray.java
@@ -19,5 +19,14 @@ public class rellenararray {
             array[i] = entrada.nextInt(20) + 1; 
         }
     }
+        
+    public void imprimirarray() {
+    for (int i = 0; i < array.length; i++) {
+        System.out.println("Valor: " + array[i]);
+    }
+    
+    public void nuevometodo1() {
+    System.out.println("Metodo 1");
+}
 }
 
